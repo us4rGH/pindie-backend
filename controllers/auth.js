889a3b1372503dcd095e34e308c1f9c40a43f1e8
us4rGH <150263960+us4rGH@users.noreply.github.com>
2024-05-18 22:23:07 +0300

    feat: added authorization router

---
+++ controllers/auth.js
@@ -0,0 +1,17 @@
+const users = require("../models/user.js");
+const bcrypt = require("bcryptjs");
+
+const login = (req, res) => {
+    const { email, password } = req.body;
+    users.findUserByCredentials(email, password)
+        .then((user) => {
+            res
+                .status(200)
+                .send({ _id: user._id, username: user.username, email: user.email });
+        })
+        .catch(error => {
+            res.status(401).send({ message: error.message });
+        });
+};
+
+module.exports = { login };
